2a0f26e5bdcfa0cb4196b26a42f277bad468836a
Sondre <sjohnsen.valle@gmail.com>
2022-02-05 21:08:31 +0100

    live wave generator is now working

--- Source/stewart_platform/Private/wave_generator/wave_generator.cpp
+++ Source/stewart_platform/Private/wave_generator/wave_generator.cpp
@@ -26,22 +26,25 @@ bool Fwave_generator::Init(){
 
 uint32 Fwave_generator::Run(){
 	float dt = 0;
-	float time_step = 0.01;
+	float time_step = 0.008333f;
 	float wave_heave = 0.0f;
 	float phase_heave = 0.0f;
-	TArray<float> w = {0.2f, 0.4f, 0.6f, 0.8f, 1.0f, 1.2f, 1.4f, 1.6f, 1.8f, 2.0f};
+	TArray<float> w = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2.0f};
 	
 	while (!_stop_thread){
+		// float s_pm = jonswap_wave_spectrum(1.0f, 8.0f, 10.0f, 1.0f);  // TODO there is some sort of bug with jonswap calculation
+		// UE_LOG(LogTemp, Warning, TEXT("s_jon: %f"), s_pm);
 		float world_time = _test->GetWorld()->GetRealTimeSeconds();
-		float ttime = irregular_wave(world_time, w, 8.0f, 10.0f, 0, 0, wave_heave, phase_heave);
+		float ttime = irregular_wave(world_time, w, 4.0f, 8.0f, 0, 0, wave_heave, phase_heave);
 		UE4_tmux.Lock();
-		position_ = {0,0,static_cast<float>(wave_heave*cos(phase_heave/180.0f*PI))/10};
+		// position_ = {0,0,static_cast<float>(static_cast<float>(3*sin(world_time)))};
+		position_ = {0,0,5.0f*wave_heave};
 		// position_ = {static_cast<float>(3*sin(world_time)), static_cast<float>(3*sin(world_time)), static_cast<float>(3*sin(world_time))};
 		rotation_ = {0,0,0};
 		// rotation_ = {static_cast<float>(10*sin(world_time)), static_cast<float>(10*sin(world_time)), static_cast<float>(10*sin(world_time))};
 		UE4_tmux.Unlock();
 		dt = _test->GetWorld()->GetRealTimeSeconds() - world_time;
-		// UE_LOG(LogTemp, Warning, TEXT("world time: %f, and dt: %f"), world_time, dt);
+		UE_LOG(LogTemp, Warning, TEXT("world time: %f, and dt: %f"), world_time, dt);
 		if (dt < time_step){
 			FPlatformProcess::Sleep(time_step-dt);	
 			
@@ -69,23 +72,28 @@ void Fwave_generator::get_pose(FVector& position, FRotator& rotation)
 
 float Fwave_generator::pierson_moskowitz_wave_spectrum(float w, float H_s, float T_p){
 	float w_p = 2.0f * PI / T_p;
-	float s_pm = 5.0f/16.0f * pow(H_s, 2) * pow(w_p, 4) * pow(w,-5) * exp(-5.0f/4.0f * pow((w/w_p),4));
+	float s_pm = 5.0f/16.0f * pow(H_s, 2) * pow(w_p, 4) * pow(w,-5) * exp(-5.0f/4.0f * pow((w/w_p),-4.0f));
 	return s_pm;
 }
 
 float Fwave_generator::jonswap_wave_spectrum(float w, float H_s, float T_p, float y){
 	float peak_shape_parameter = T_p / sqrt(H_s);
+	float applied_y = 0;  // TODO see if this can be substituted by y
 	if (y == 0){
 		if (peak_shape_parameter <= 3.6f){
-			y = 5.0f;
+			applied_y = 5.0f;
 		}
 		else if (peak_shape_parameter >= 5.0f){
-			y = 1.0f;
+			applied_y = 1.0f;
 		}
 		else {
-			y = exp(5.75f - 1.15f * T_p/sqrt(H_s));
+			applied_y = exp(5.75f - 1.15f * T_p/sqrt(H_s));
 		}
 	}
+	else{
+		applied_y = y;
+	}
+	
 	float w_p = 2 * PI / T_p;
 	float sigma = 0;
 	if (w <= w_p){
@@ -94,15 +102,15 @@ float Fwave_generator::jonswap_wave_spectrum(float w, float H_s, float T_p, floa
 	else{
 		sigma = 0.09f;
 	}
-	float a_y = 1.0f - 0.0287f * log(y);
-	float s_jonswap = a_y * pow(a_y, exp(-0.5f*pow(((w-w_p)/(sigma*w_p)),2))) * pierson_moskowitz_wave_spectrum(w, H_s, T_p);
+	float a_y = 1.0f - 0.0287f * log(applied_y);
+	float s_jonswap = a_y * pow(applied_y, exp(-0.5f*pow((w-w_p)/(sigma*w_p),2))) * pierson_moskowitz_wave_spectrum(w, H_s, T_p);
 	return s_jonswap;
 }
 
-float Fwave_generator::irregular_wave(float t, TArray<float> w, float H_s, float T_p, float y, float x, float& amplitude, float& phase){
+float Fwave_generator::irregular_wave(float t, TArray<float> w, float H_s, float T_p, float y, float x, float& amplitude, float& phase){  // TODO update function to correct parameters
 	
 	// int vector_length = sizeof(w.Num());
-	int vector_length =10;
+	int vector_length =20; // TODO get this form the vector
 	
 	// for (int i =0; i < vector_length; i++)
 	// {
@@ -112,8 +120,9 @@ float Fwave_generator::irregular_wave(float t, TArray<float> w, float H_s, float
 	// 	UE_LOG(LogTemp, Warning, TEXT("Size of frequency vector: %i, value of step[%i]: %f"), vector_length, i, w[i]);
 	// 	
 	// }
-	
-	
+	// FRandomStream Stream(FMath::Rand());
+	FRandomStream Stream(15);
+	float surface_elevation = 0;
 	float delta_w = 0.0f;
 	float amplitude_i = 0.0f;
 	float phase_i = 0.0f;
@@ -124,11 +133,17 @@ float Fwave_generator::irregular_wave(float t, TArray<float> w, float H_s, float
 		else{
 			delta_w = w[i] - w[i-1];
 		}
-		phase_i = phase_i +  (w[i]*t + FMath::FRandRange(0.0f,2*PI));  // TODO add support for k_i*x
-		amplitude_i = amplitude_i + sqrt(2.0f*delta_w*jonswap_wave_spectrum(w[i], H_s, T_p, y));
+		// phase_i =  w[i]*t;  // TODO add support for k_i*x
+		phase_i =  (w[i]*t + Stream.FRandRange(0.0f,2*PI));  // TODO add support for k_i*x
+		// phase_i = phase_i +  (w[i]*t + FMath::FRandRange(0.0f,2*PI));  // TODO add support for k_i*x
+		amplitude_i = sqrt(2.0f*delta_w*jonswap_wave_spectrum(w[i], H_s, T_p, y));
+		surface_elevation += amplitude_i * cos(phase_i);
+		
 	}
-	amplitude = MoveTemp(amplitude_i);
+	// amplitude = MoveTemp(amplitude_i);
+	amplitude = MoveTemp(surface_elevation);
 	phase = MoveTemp(phase_i);
+	
 		UE_LOG(LogTemp, Warning, TEXT("Amplitude: %f , Phase: %f"), amplitude_i, phase_i);
 	return t;
 	// return 0;
